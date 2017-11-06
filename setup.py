6449c34b0be2f83cc02ffe0ab849e734fa877259
Andrew Leitsius <leits@quintagroup.com>
2017-11-06 12:45:58 +0200

    Fix setup.py requires

--- setup.py
+++ setup.py
@@ -31,26 +31,27 @@ entry_points = {
     ]
 }
 
-setup(name='openregistry.convoy',
-      version=version,
-      description="openregistry.convoy",
-      long_description='openregistry.convoy',
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-          "Programming Language :: Python",
-      ],
-      keywords='Bot',
-      author='Quintagroup, Ltd.',
-      author_email='info@quintagroup.com',
-      url='https://github.com/openprocurement/openregistry.convoy',
-      license='Apache License 2.0',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['openregistry'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_requires=test_requires,
-      extras_requires=extras_requires,
-      entry_points=entry_points,
-      )
+setup(
+    name='openregistry.convoy',
+    version=version,
+    description="openregistry.convoy",
+    long_description='openregistry.convoy',
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+      "Programming Language :: Python",
+    ],
+    keywords='Bot',
+    author='Quintagroup, Ltd.',
+    author_email='info@quintagroup.com',
+    url='https://github.com/openprocurement/openregistry.convoy',
+    license='Apache License 2.0',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['openregistry'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require=extras_requires,
+    entry_points=entry_points,
+
+)
