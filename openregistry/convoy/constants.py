2a6f785848a24888c8760b5a09cc47cd2ddb2fc5
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-02-26 18:50:57 +0200

    Update convoy.yaml. Make file with constants

---
+++ openregistry/convoy/constants.py
@@ -0,0 +1,73 @@
+DEFAULTS = {
+    "timeout": 10,
+    "transmitter_timeout": 15,
+    "couchdb": {
+        "user": "",
+        "password": "",
+        "host": "127.0.0.1",
+        "port": "5984",
+        "db": "ea_auctions",
+    },
+    "cdb_ds": {
+        "host_url": "http://docs-sandbox.ea.openprocurement.org",
+        "auth_ds": ["", ""]
+    },
+    "cdb": {
+        "key": "",
+        "host_url": "http://127.0.0.1:6543",
+        "api_version": 2.5,
+        "user_agent": "auctions_convoy",
+        "resource": "auctions"
+    },
+    "lots_db": {
+        "key": "convoy",
+        "host_url": "http://0.0.0.0:6543",
+        "api_version": 0.1,
+        "user_agent": "lots_convoy"
+    },
+    "assets_db": {
+        "key": "convoy",
+        "host_url": "http://0.0.0.0:6543",
+        "api_version": 0.1,
+        "user_agent": "assets_convoy"
+    },
+    "version": 1,
+    "formatters": {
+        "simple": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "simple",
+            "stream": "ext://sys.stdout"
+        },
+        "statsd": {
+            "class": "logging.StatsdHandler",
+            "level": "DEBUG",
+            "config_path": "path/to/statsd/config"
+        }
+    },
+    "loggers": {
+        "openregistry.convoy": {
+            "handlers": ["console", "statsd"],
+            "propagate": "no",
+            "level": "DEBUG"
+        },
+        "logger_exc_logger": {
+            "level": "ERROR",
+            "handlers": None,
+            "qualname": "exc_logger",
+            "propagate": 0
+        },
+        "": {
+            "handlers": ["console", "statsd"],
+            "level": "DEBUG"
+        }
+    }
+}
+
+DOCUMENT_KEYS = ['hash', 'description', 'title', 'url', 'format', 'documentType']
+KEYS = ['classification', 'additionalClassifications', 'address', 'unit', 'quantity', 'location', 'id']
