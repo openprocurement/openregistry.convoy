6784f5f122890b30b444cfbe478be47e2726b540
Andrew Leitsius <leits@quintagroup.com>
2018-08-23 18:05:03 +0300

    Add documents to CONTRACT_NOT_REQUIRED_FIELDS

--- openregistry/convoy/loki/constants.py
+++ openregistry/convoy/loki/constants.py
@@ -4,9 +4,9 @@ UNSUCCESSFUL_TERMINAL_STATUSES = ('cancelled', 'unsuccessful')
 CONTRACT_REQUIRED_FIELDS = [
     'awardID', 'contractID', 'items', 'suppliers',
     'value', 'dateSigned',
-    #'documents'
 ]
 CONTRACT_NOT_REQUIRED_FIELDS = [
     'contractNumber', 'title', 'title_en', 'title_ru',
-    'description', 'description_en', 'description_ru'
+    'description', 'description_en', 'description_ru',
+    'documents'
 ]
